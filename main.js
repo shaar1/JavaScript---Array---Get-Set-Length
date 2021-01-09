7f064884dabeb2f1101b730febded276105abd22
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 15:55:45 +0100

    träning om Array med length

---
+++ main.js
@@ -0,0 +1,46 @@
+/* global console, alert, prompt*/
+/*
+var friendOne = "Hassan",
+
+friendTwo = "Soha",
+friendThree = "Ahmad",
+friendFour = "Zahra",
+friendFive = "Hind";
+
+var friends = [
+"Hassan", 
+"Soha",  
+"Ahmad",  
+"Zahra",  
+"Hind"    
+];
+
+console.log(friends);
+
+var myVariable = "Hello JavaScript";
+
+console.log(myVariable.length);
+
+*/
+
+var friendOne = "Hassan",
+
+friendTwo = "Soha",
+friendThree = "Ahmad",
+friendFour = "Zahra",
+friendFive = "Hind";
+
+var friends = [
+"Hassan", 
+"Soha",  
+"Ahmad",  
+"Zahra",  
+"Hind"    
+];
+
+console.log(friends.length);
+
+
+
+
+

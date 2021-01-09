94415e1c3a1b6362f5de61820b2e44fde1bed3c9
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 16:01:22 +0100

    Arrays träning med get och setnlength

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var myVariable = "Hello JavaScript";
 console.log(myVariable.length);
 
 */
-
+/*
 var friendOne = "Hassan",
 
 friendTwo = "Soha",
@@ -39,8 +39,56 @@ var friends = [
 ];
 
 console.log(friends.length);
+*/
+/*
+var friendOne = "Hassan",
 
+friendTwo = "Soha",
+friendThree = "Ahmad",
+friendFour = "Zahra",
+friendFive = "Hind";
 
+var friends = [
+"Hassan", 
+"Soha",  
+"Ahmad",  
+"Zahra",  
+"Hind"    
+
+];
+
+console.log(friends.length); // Get Length
+
+console.log(friends);
+ 
+friends.length = 2; // Set length To 2
+
+console.log(friends);
+*/
+
+var friendOne = "Hassan",
+
+friendTwo = "Soha",
+friendThree = "Ahmad",
+friendFour = "Zahra",
+friendFive = "Hind";
+
+var friends = [
+"Hassan", 
+"Soha",  
+"Ahmad",  
+"Zahra",  
+"Hind"    
+
+];
+
+console.log(friends.length); // Get Length
+
+console.log(friends);
+ 
+friends.length = 5; // Set length To 5
+
+console.log(friends);
 
 
 
